7d89b5cfc7dfa5e359c9d9be9dc82289af332609
Piriyapong Laopongsit <peawyoyoyin@gmail.com>
2019-03-30 13:07:35 +0700

    Update Navbar

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 
+import { Navbar as BSNavbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+
 class Navbar extends Component {
   render() { 
     return (
-      <nav>
-        Navbar
-      </nav>
+      <BSNavbar bg="dark" variant="dark justify-content-between">
+        <BSNavbar.Brand> Fun D </BSNavbar.Brand>
+        <Nav className="justify-content-end">
+          <Form inline>
+            <FormControl type="text" placeholder="Search Jobs" className="mr-sm-2" />
+            <Button variant="outline-light">Search</Button>
+          </Form>
+
+          <Nav.Link>Notifications</Nav.Link>
+          <Nav.Link>Profile</Nav.Link>
+        </Nav>
+      </BSNavbar>
     );
   }
 }
