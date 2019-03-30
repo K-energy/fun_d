f07955a2eeaf1f8e6613e491052607c07749e73c
ThipokKub <thipok30386@gmail.com>
2019-03-30 13:03:44 +0700

    Add font-awesome

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
           <Route exact path="/" component={() => (
             <section>
               <h1>This is Fun D</h1>
+              <i className="fa fa-bars" />
             </section>
           )}/>
         </BrowserRouter>
