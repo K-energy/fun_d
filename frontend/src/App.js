f23f55898c996ce550e75f962959fa0d59991143
ThipokKub <thipok30386@gmail.com>
2019-03-30 22:21:24 +0700

    Remove index page concatenate with error page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { Home, Detail, CompanyProfile, NotImplemented } from "./pages"
 import TemplateWrapper from "./helpers/TemplateWrapper";
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
@@ -10,10 +10,12 @@ class App extends Component {
       <>
         { /* <Navbar /> */ }
         <BrowserRouter>
-          <Route exact path="/" component={TemplateWrapper(Home)}/>
-          <Route exact path="/detail" component={TemplateWrapper(Detail)}/>
-          <Route exact path="/profile" component={TemplateWrapper(CompanyProfile)}/>
-          <Route component={TemplateWrapper(NotImplemented)} />
+          <Switch>
+            <Route exact path="/" component={TemplateWrapper(Home)}/>
+            <Route exact path="/detail" component={TemplateWrapper(Detail)}/>
+            <Route exact path="/profile" component={TemplateWrapper(CompanyProfile)}/>
+            <Route component={TemplateWrapper(NotImplemented)} />
+          </Switch>
         </BrowserRouter>
       </>
     );
