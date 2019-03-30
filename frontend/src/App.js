f70f3d4acaab6a8a1c6b5c2a2627dde190827ea3
Piriyapong Laopongsit <peawyoyoyin@gmail.com>
2019-03-30 12:50:18 +0700

    add navbar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar';
 
 class App extends Component {
   render() {
     return (
-      <section>
-        <h1>This is Fun D</h1>
-      </section>
+      <>
+        <Navbar></Navbar>
+        <section>
+          <h1>This is Fun D</h1>
+        </section>
+      </>
     );
   }
 }
