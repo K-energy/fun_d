b18f29bae761c8ee8dd6ef254463788a725611ef
Piriyapong Laopongsit <peawyoyoyin@gmail.com>
2019-03-30 12:53:31 +0700

    add basic react router structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar';
+
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <>
         <Navbar></Navbar>
-        <section>
-          <h1>This is Fun D</h1>
-        </section>
+        <BrowserRouter>
+          <Route exact path="/" component={() => (
+            <section>
+              <h1>This is Fun D</h1>
+            </section>
+          )}/>
+        </BrowserRouter>
       </>
     );
   }
